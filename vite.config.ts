0ce3b7dc2a5b510dbf83481f40a2a47f06780a32
kamalproyek19 <kamalproyek19@gmail.com>
2026-06-01 12:53:10 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/absensi-imtihan/',
+  base: '/absensiimtihan/',
 
   plugins: [
     react(),
